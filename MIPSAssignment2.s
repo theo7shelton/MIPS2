4f173ab9121d9de72f97a1563a241b9a91aeb0c3
theo7shelton <19628883+theo7shelton@users.noreply.github.com>
2017-11-30 04:16:16 -0500

    All subprograms and main
    Everything works as intended except for spaces. Once a space is entered, it outputs "NaN" which is incorrect.

--- MIPSAssignment2.s
+++ MIPSAssignment2.s
@@ -26,70 +26,18 @@ main:							# Begin main code
 	la $a0, buffer					# load buffer address into $a0
 	li $a1, 1001					# define amount of characters to be read
 	syscall						# system call for keyboard input
-	li $s1, 0					# Initialize register for use
-	sub $s7, $a0, 1					# Stores address of byte before string buffer
 	
-     		# Loop until endcharacter (carriage return or end of line or COMMA*)
-inputLoop:
-	
-	li $t9, 0					
-	li $t8, 0					# Counts amount of valid characters in string
-	la $t7, validCharacters				# Load address in memory to store valid characters
-    
-
-
-		addiu $t8, $t8, 1				# Keep track of valid character entries 
-		bgt $t8, 9, tooManyChars			# Leave loop string is greater than 8 valid characters
-		sb $t0, 0($t7)					# Move valid character into validCharacter array
-		addiu $t7, $t7, 1				# Put address of next byte of validCharacter array in $t7
-		li $s1, 0					# Reset these registers
-		li $s0, 0
-
-		j checkString					# Loop back to checkString
-		
-		# First check each character in a single word
-		# If characters are valid, call convertString to convert single string and call displayResult to display the result
-		# loop function
-endCheckComma:
-	
-	beqz $t8, inputInvalid				# If no valid characters entered, input is invalid
-	# valid: 
-		# Call sub
-		addi $sp, $sp, -9				# Enough space for 8 characters 
-		
-		j goToLoop
-
-	inputInvalid:
-		li $t1, 1
-		addi $sp, $sp, -1
-		sb $t1, 0($sp)
-		jal subprogram_3
-		addi $sp, $sp, 1
-		j  goToLoop
-tooManyChars:
-	li $t1, 2
-	addi $sp, $sp, -1
-	sb $t1, 0($sp)
-	jal subprogram_3
-	addi $sp, $sp, 1
-
-goToLoop:
-	li $v0, 4				# load print string call code
-	la $a0, char_comma			# Print comma
-	syscall					# print buffer string
-	j inputLoop
-
-endInputLoop:
-	beqz $t8, noInput
-	valid:
-	
-	noInput:
-	li $t1, 1
-	addi $sp, $sp, -1
-	sb $t1, 0($sp)
-	jal subprogram_3
-	addi $sp, $sp, 1	
-	
+getNextString:
+	jal subprogram_2
+	lbu $t0, 1($sp)					# Check if last string conversion
+	beq $t0, 1, endGetNextString			# ^^^
+	jal subprogram_3				# Call display subprogram and pass values in stack
+	lw $a0, 6($sp)					# Get starting position of next string
+	addiu $sp, $sp, 10
+	j getNextString
+endGetNextString:
+	jal subprogram_3				# Call display subprogram and pass values in stack
+	addiu $sp, $sp, 10
 endFunc:
 	li $v0, 10
 
@@ -98,6 +46,14 @@ endFunc:
 						# System call to exit
 
 
+##################################################################################################################	
+# 	########################## Subprogram_1 ##################################
+#	Converts string to decimal.
+# 	Returns 0 if string was too large, 1 if string is NaN, and 2 if string is valid (first byte in stack)
+# 	Returns 1 if all strings were converted (second byte in stack)
+# 	Returns unsigned decimal in stack (4 bytes in stack)
+# 	Returns starting address of next string (4 bytes in stack)
+##################################################################################################################
 subprogram_1:						# hexToDec
 	
 	li $s0, 0					# Reset these registers
@@ -105,43 +61,52 @@ subprogram_1:						# hexToDec
 
      checkChar:
 	li $t1, 0x0000003A
-	slt $t2, $a0, $t1				# Sets $t2 if t0 <3Ah
+	slt $t2, $a1, $t1				# Sets $t2 if t0 <3Ah
 	li $t1, 0x0000002F				
-	slt $t3, $t1, $a0				# Sets $t3 if $t0 > 2Fh
+	slt $t3, $t1, $a1				# Sets $t3 if $t0 > 2Fh
 	and $s0, $t2, $t3				# Sets $s0 if byte is in range of 0-9 ASCII 
 	addu $s1, $s1, $s0				# If falls in the range, increments $s1
 	bne $s1, 1, checkAThroughF
     	li $v0, 2					# Set to 2 to show that inputted character is valid
-	subu $v1, $t2, 48
+	addi $v1, $a1, -48
 	jr $ra
 	
      checkAThroughF:					# Check if byte has code for ASCII A-F
 	li $t1, 0x00000047
-	slt $t2, $a0, $t1				# Sets $t2 if t0 < 47
+	slt $t2, $a1, $t1				# Sets $t2 if t0 < 47
 	li $t1, 0x00000040				
-	slt $t3, $t1, $t0				# Sets $t3 if $t0 > 40
+	slt $t3, $t1, $a1				# Sets $t3 if $t0 > 40
 	and $s0, $t2, $t3				# Sets $s0 if byte is in range of A-F ASCII 
 	addu $s1, $s1, $s0				# If falls in the range, increments $s1
 	bne $s1, 1, aThroughfCheck
 	li $v0, 2					# Set to 2 to show that inputted character is valid
-	subu $v1, $t2, 55
+	addi $v1, $a1, -55
 	jr $ra
 	
      aThroughfCheck:		# Check if byte has code for ASCII a-f 
 	li $t1, 0x00000067
-	slt $t2, $a0, $t1				# Sets $t2 if t0 < 67
+	slt $t2, $a1, $t1				# Sets $t2 if t0 < 67
 	li $t1, 0x00000060				
-	slt $t3, $t1, $a0				# Sets $t3 if $t0 > 60
+	slt $t3, $t1, $a1				# Sets $t3 if $t0 > 60
 	and $s0, $t2, $t3				# Sets $s0 if byte is in range of a-f ASCII 
 	addu $s1, $s1, $s0				# If falls in the range, increments $s1
 	bne $s1, 1, invalidInput
 	li $v0, 2					# Set to 2 to show that inputted character is valid
-	subu $v1, $t2, 87
+	addi $v1, $a1, -87
 	jr $ra
 
      invalidInput:
 	li $v0, 1					# Set $v0 to 3 if NaN
 	jr $ra		
+
+##################################################################################################################	
+# 	########################## Subprogram_2 ##################################
+#	Converts string to decimal.
+# 	Returns 0 if string was too large, 1 if string is NaN, and 2 if string is valid (first byte in stack)
+# 	Returns 1 if all strings were converted (second byte in stack)
+# 	Returns unsigned decimal in stack (4 bytes in stack)
+# 	Returns starting address of next string (4 bytes in stack)
+##################################################################################################################
 
 subprogram_2: 						# convertString
 	move $t0, $a0					# Address is in $a0
@@ -168,9 +133,10 @@ subprogram_2: 						# convertString
 	bne $t9, $zero, setNaN				# Check if there was a sequence of character, any amount of spaces, then a character. If so branch to invalid
 	jal subprogram_1				# Call subprogram_1 to figure out decimal
 	beq $v0, 1, setNaN
-	sll $s6, $s6, 1
+	sll $s6, $s6, 4
 	addu $s6, $s6, $v1
 	addi $t8, $t8, 1
+	j jumpToLoop
      setNaN:
 	li $t7, 1
 	j jumpToLoop
@@ -182,6 +148,7 @@ subprogram_2: 						# convertString
     endProgram:
 	li $t6, 1
     exitLoop:
+	addiu $t0, $t0, 1				# Get next string starting address in case needed
 	beq $t7, 1, exitFunc
 	bge $t8, 9, setTooLarge
 	beqz $t8, setNoInput
@@ -189,25 +156,35 @@ subprogram_2: 						# convertString
     setTooLarge:
 	li $t7, 0
 	j exitFunc
-    setNoInput
+    setNoInput:
 	li $t7, 1
     exitFunc: 
-	addi $sp, $sp, -6
-	sb $t7, 0($sp)
-	sb $t6, 1($sp)
+	addi $sp, $sp, -10
+	sb $t7, 0($sp)					# Return if string is valid, too large or NaN
+	sb $t6, 1($sp)					# Return if end of string
 	sw $s6, 2($sp)
+	sw $t0, 6($sp)					# Return next string starting address
 	move $ra, $s7
 	jr $ra
-	
 
+
+	
+##################################################################################################################	
+# 	########################## Subprogram_3 ##################################
+#	Converts string to decimal.
+# 	Returns 0 if string was too large, 1 if string is NaN, and 2 if string is valid (first byte in stack)
+# 	Returns 1 if all strings were converted (second byte in stack)
+# 	Returns unsigned decimal in stack (4 bytes in stack)
+# 	Returns starting address of next string (4 bytes in stack)
+##################################################################################################################
 subprogram_3:						# displayResult:
 	
 		# Take first single byte from stack, if 1 then should display value passed in stack. 
 		# If zero, jump to invalid and display invalid_string "NaN"
 	lbu $t0, 0($sp)					# Check first byte of stack, if 1, print NaN, if 2 Too large, if 0
-	beq $t0, 2, tooLarge
+	beqz $t0, tooLarge
 	beq $t0, 1, notANumber
-	lw $t0, 1($sp) 					# Move 4 byte long number to $t0
+	lw $t0, 2($sp) 					# Move 4 byte long number to $t0
 	bgez $t0, showInt				# If number is positive, show integer
 	li $t1, 10					
 	divu $t0, $t1					# Divide number by 10
